bedfc083a656ffd276a2aad9fd66920dff70f0cd
dimiarbre <dimitri.lereverend@gmail.com>
2024-10-19 11:11:33 +0200

    Switched to an arrow display for gets. Will need to change buffering so that it indicates change in direction, showing full range of indices taken into account.

--- src/algovisim/VisualTypes/VisualArray.py
+++ src/algovisim/VisualTypes/VisualArray.py
@@ -17,13 +17,14 @@ class VisualArray(list):
         self.nb_iterators = 0
         # Will buffer accesses of the same type to speed up and display concurrent accesses.
         self.buffer_animations = []
+        self.buffer_clear = []  # Clearing animations, to make boxes and such dissapear.
         self.buffer_type: ArrayAnimationType = "get"
 
         self.old_tab = copy.deepcopy(self.tab)
         self.old_tab_counter = Counter(self.old_tab)
 
-        # self.arrow = Arrow(UP,DOWN)
-        # self.arrow.shift()
+        self.arrow = None
+        self.arrow_buffer = []
 
     def _draw(self, now=True) -> Transform:
         """Redraw the array, and try to make a permutation if possible.
@@ -53,31 +54,58 @@ class VisualArray(list):
         return len(self.tab)
 
     def flush_buffer(self):
-        to_play = [Write(obj) for obj in self.buffer_animations]
+        # to_play = [Write(obj) for obj in self.buffer_animations]
         if self.buffer_type == "set":
-            to_play.append(self._draw(now=False))
-        self.scene.play(*to_play)  # Play buffered animations concurrently
-        self.scene.play(*[FadeOut(obj) for obj in self.buffer_animations])
+            self.buffer_animations.append(self._draw(now=False))
+        if len(self.buffer_animations) > 0:
+            self.scene.play(
+                *self.buffer_animations
+            )  # Play buffered animations concurrently
+        if len(self.buffer_clear) > 0:
+            self.scene.play(*self.buffer_clear)
         self.buffer_animations = []
+        self.buffer_clear = []
 
-    def _add_to_buffer(self, animation, animation_type: ArrayAnimationType):
+    def _add_to_buffer(
+        self, animation, animation_type: ArrayAnimationType, clear_animation=None
+    ):
         if animation_type != self.buffer_type:
             self.flush_buffer()
             self.buffer_type = animation_type
-        self.buffer_animations.append(animation)
+        if animation is not None:
+            self.buffer_animations.append(animation)
+        if clear_animation is not None:
+            self.buffer_clear.append(clear_animation)
 
     def __getitem__(self, i):
         if i < 0:
             i = len(self) + i
-        box = self.box_around(i, i)
-        self._add_to_buffer(box, "get")
+        arrow_animation = self._new_arrow_position(i)
+        self._add_to_buffer(arrow_animation, "get")
         return self.tab[i]
+
+    def _new_arrow_position(self, i):
+        if self.arrow is None:
+            # When we draw an arrow for the first time
+            self.arrow = Arrow(UP, DOWN)
+            self.arrow.move_to(self.tab_object[self.get_true_index(i)])
+            # TODO: have a consistent spacing by extracting the top coordinates
+            self.arrow.shift(1.2 * UP)
+            self.scene.add(self.arrow)
+            return None
+        else:
+            new_arrow = Arrow(UP, DOWN)
+            new_arrow.move_to(self.tab_object[self.get_true_index(i)])
+            # TODO: have a consistent spacing by extracting the top coordinates
+            new_arrow.shift(1.2 * UP)
+            self.arrow.target = new_arrow
+            return MoveToTarget(self.arrow)
 
     def __setitem__(self, i, x):
         if i < 0:
             i = len(self) + i
         box = self.box_around(i, i, "red")
-        self._add_to_buffer(box, "set")
+        self._add_to_buffer(Write(box), "set", clear_animation=FadeOut(box))
         self.tab[i] = x
 
     def _index(self, i):
@@ -99,7 +127,7 @@ class VisualArray(list):
 class VisualArrayExample(Scene):
     def construct(self):
         random.seed(421)
-        random_tab = [i for i in range(7)]
+        random_tab = [i for i in range(10)]
         random.shuffle(random_tab)
         tab = VisualArray(random_tab, self)
 
